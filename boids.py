865cdee5313cac386dc056179337a0e0813c1ffa
05watson.j@gmail.com <05watson.j@gmail.com>
2017-01-09 13:53:20 +0000

    Replaced some hard coded numbers with variables

--- boids.py
+++ boids.py
@@ -3,37 +3,44 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
 
-# Deliberately terrible code for teaching purposes
+#Introduce variables to remove hard coded variables.
+Num_boids = 50  # Number of boids.
 
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
+
+
+boids_x=[random.uniform(-450,50.0) for x in range(Num_boids)]
+boids_y=[random.uniform(300.0,600.0) for x in range(Num_boids)]
+boid_x_velocities=[random.uniform(0,10.0) for x in range(Num_boids)]
+boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(Num_boids)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
     xs,ys,xvs,yvs=boids
+    
     # Fly towards the middle
-    for i in range(50):
-        for j in range(50):
-            xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-    for i in range(50):
-        for j in range(50):
+    for i in range(Num_boids):
+        for j in range(Num_boids):
+            xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs) 
+    for i in range(Numboids):
+        for j in range(Numboids):
             yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
+            
     # Fly away from nearby boids
-    for i in range(50):
-        for j in range(50):
-            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
+    for i in range(Num_boids):
+        for j in range(Num_boids):
+            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:  #If distance between two boids is sufficiently small, moves them away.
                 xvs[i]=xvs[i]+(xs[i]-xs[j])
                 yvs[i]=yvs[i]+(ys[i]-ys[j])
+                
     # Try to match speed with nearby boids
-    for i in range(50):
-        for j in range(50):
+    for i in range(Num_boids):
+        for j in range(Num_boids):
             if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
                 xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
                 yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+                
     # Move according to velocities
-    for i in range(50):
+    for i in range(Num_boids):
         xs[i]=xs[i]+xvs[i]
         ys[i]=ys[i]+yvs[i]
 
